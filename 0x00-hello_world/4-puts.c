0c57ebe7bb4cc11028a116abc0d2bf6a7d7fde96
Ahmed Eldeeb <eldeeb757575@gmail.com>
2023-02-16 18:36:45 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,12 @@
 #include <stdio.h>
+/**
+ * main - the entry point of the code
+ *
+ * Description: Here the main function just uses
+ * the function puts to print some statment
+ *
+ * Return: it returns zero at the completeion success
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
