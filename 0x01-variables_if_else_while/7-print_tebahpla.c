e2dff382d74858b351e808b7262857458186af96
Ahmed Eldeeb <eldeeb757575@gmail.com>
2023-02-17 11:56:52 -0800

    mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - the entry point
+ *
+ * Description: a program that prints the alphabet in lowercase,
+ * followed by a new line.
+ *
+ * Return: returns zero once completion successfully
+ */
+
+int main(void)
+{
+	char alph[] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 26; i > 0; i--)
+	{
+		putchar(alph[i]);
+	}
+	putchar('\n');
+	return (0);
+}
