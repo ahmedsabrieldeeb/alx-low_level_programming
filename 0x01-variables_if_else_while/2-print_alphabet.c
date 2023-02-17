89371825afd1eacd3e1f45172049829d695ae3a2
Ahmed Eldeeb <eldeeb757575@gmail.com>
2023-02-17 11:01:59 -0800

    alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - the entry point
+ *
+ * Description: a program that prints the alphabet in lowercase,
+ * followed by a new line.
+ *
+ * Return: returns zero once completion successfully
+ */
+
+int main(void)
+{
+	char alph[] = "abcdefghijklmnopqrstuvwxyz";
+
+	for (int i; i < 26; i++)
+	{
+		putchar(alph[i]);
+	}
+	putchar('\n');
+	return (0);
+}
