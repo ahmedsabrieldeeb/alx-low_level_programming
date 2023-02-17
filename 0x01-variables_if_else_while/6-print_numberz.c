15451cd3856987a2dad64bdc385628b8dd257b1b
Ahmed Eldeeb <eldeeb757575@gmail.com>
2023-02-17 11:41:52 -0800

    base 10 without printf

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - the entry point
+ *
+ * Description: prints all single digit numbers of base 10
+ * starting from 0 using putchar, followed by a new line.
+ *
+ * Return: returns zero once completion successfully
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(char(alph[i]));
+	}
+	putchar('\n');
+	return (0);
+}
