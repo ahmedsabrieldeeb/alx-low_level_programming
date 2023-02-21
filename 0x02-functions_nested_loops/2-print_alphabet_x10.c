3fa3dc5186b21f1317648b25f8a47021507c5d5e
Ahmed Eldeeb <eldeeb757575@gmail.com>
2023-02-21 10:06:14 -0800

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - the entry point
+ *
+ * Description: a program that prints the alphabet in lowercase
+ * ten times followed by a new line.
+ *
+ * Return: returns nothing
+ */
+
+void print_alphabet_x10(void)
+{
+	char alph[] = "abcdefghijklmnopqrstuvwxyz";
+	int i, j;
+
+	for (j = 0; j < 10; j++)
+	{	
+		for (i = 0; i < 26; i++)
+		{
+			_putchar(alph[i]);
+		}
+		_putchar('\n');
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 int _putchar(char c);
 void print_alphabet(void);
+oid print_alphabet_x10(void);
 
 #endif /* MAIN_H */
