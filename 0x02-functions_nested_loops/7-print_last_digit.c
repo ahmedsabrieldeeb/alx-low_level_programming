76477211d3eb22cacb05c4bf0ce07629f950e98d
Ahmed Eldeeb <eldeeb757575@gmail.com>
2023-02-21 11:36:43 -0800

    last

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -13,6 +13,15 @@ int print_last_digit(int n)
 	int res;
 
 	res = n - (n / 10) * 10;
-	_putchar(res + '0');
-	return (res);
+
+	if (res >= 0)
+	{
+		_putchar(res + '0');
+		return (res);
+	}
+	else
+	{
+		_putchar((-1 * res) + '0');
+		return (-1 * res);
+	}
 }
