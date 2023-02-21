ed8bfe059c48bffcb96666149188db717d5a4c9b
Ahmed Eldeeb <eldeeb757575@gmail.com>
2023-02-21 09:02:54 -0800

    insomnia

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,19 +1,2 @@
-#include <unistd.h>
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
 int _putchar(char c);
-/**
- * print_alphabet - the entry point
- *
- * Description: a program that prints the alphabet in lowercase,
- * followed by a new line.
- *
- * Return: return nothing "void"
- */
 void print_alphabet(void);
