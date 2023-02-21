a3f73f6b0f3eb4563dc40fb19a60338cb912f291
Ahmed Eldeeb <eldeeb757575@gmail.com>
2023-02-21 11:09:45 -0800

    last

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_last_digit - the entry point
+ * @n: the number to be extracted from
+ * Description: a function that prints the last digit of a number.
+ *
+ * Return: returns the lsat digit
+ */
+
+int print_last_digit(int n)
+{
+	return (n - (n / 10) * 10);
+}
