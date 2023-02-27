c8cb33345191258d2cd22532130e21ef2a5041b7
Ahmed Eldeeb <eldeeb757575@gmail.com>
2023-02-27 22:56:21 +0000

    reverse string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,42 @@
+#include "main.h"
+/**
+ * _strlen - a function that returns the length of a string.
+ * @s: the string to be calculated for its length
+ *
+ * Return: int value contains the length
+ */
+
+int _strlen(char *s)
+{
+	int i, len;
+
+	i = 0;
+	len = 0;
+
+	while (*(s + i) != '\0')
+	{	len++;
+		i++;
+	}
+
+	return (len);
+}
+/**
+ * print_rev - a function that prints a string, in reverse,
+ * followed by a new line.
+ * @s: the string to be reversed
+ *
+ * Return: void.
+ */
+void print_rev(char *s)
+{
+	int len;
+
+	len = _strlen(s) - 1;
+
+	while (len >= 0)
+	{
+		_putchar(*(str + len));
+		len--;
+	}
+	_putchar('\n');
+}
