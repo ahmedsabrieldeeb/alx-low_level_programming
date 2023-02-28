df13c5fa34ded72f31863a084e1834227df1f8d3
Ahmed Eldeeb <eldeeb757575@gmail.com>
2023-02-28 00:25:06 +0000

    up to n

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -15,10 +15,10 @@ void print_array(int *a, int n)
 
 	i = 0;
 
-	while (*(a + i) < (n - 1))
+	while (i < (n - 1))
 	{
 		printf("%d, ", *(a + i));
 		i++;
 	}
-	printf("%d\n", *(a + (++i)));
+	printf("%d\n", *(a + i));
 }
