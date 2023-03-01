44cc69538d8e7698e95b22936dea43c2ade1c9ac
Ahmed Eldeeb <eldeeb757575@gmail.com>
2023-03-01 23:56:10 +0000

    my own strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strncpy - a function that copies a string.
+ * @dest: the destination string to be concatenated to
+ * @src: the source string to concatenate
+ * @n: the most bytes to concatenate to
+ *
+ * Description: it will use at most n bytes from src
+ * and src does not need to be null-terminated if it contains n or more bytes
+ *
+ * Return: a pointer to the resulting string dest.
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i, j;
+
+	j = 0;
+	for (i = 0; i < n; i++)
+	{
+		*(dest + i) = *(src + j);
+		j++;
+		if (*(src + j) == '\0')
+		{
+			return (dest);
+		}
+	}
+	return (dest);
+}
