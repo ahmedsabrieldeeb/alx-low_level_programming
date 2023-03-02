93d7d35cfeb8296ed6ba3c04d4dfa428a105a2b8
Ahmed Eldeeb <eldeeb757575@gmail.com>
2023-03-02 00:04:11 +0000

    my own strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,14 +13,12 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i, j;
+	int i;
 
-	j = 0;
 	for (i = 0; i < n; i++)
 	{
-		*(dest + i) = *(src + j);
-		j++;
-		if (*(src + j) == '\0')
+		*(dest + i) = *(src + i);
+		if (*(src + i) == '\0')
 		{
 			return (dest);
 		}
