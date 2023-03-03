1e2c4a4378c16b646598df1c4f62b6bad2c0fdc3
Ahmed Eldeeb <eldeeb757575@gmail.com>
2023-03-03 09:56:48 +0000

    me leet code

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * leet - a function that encodes a string into 1337
+ * @s: the string to be worked on
+ *
+ * Description: Letters a and A should be replaced by 4
+ * Letters e and E should be replaced by 3
+ * Letters o and O should be replaced by 0
+ * Letters t and T should be replaced by 7
+ * Letters l and L should be replaced by 1
+ *
+ * Return: char pointer
+ */
+
+char *leet(char *s)
+{
+	int i, j;
+	int arr[15] = {97, 65, 52, 101, 69, 51, 108, 76, 49, 111, 79, 48, 116, 84, 55};
+
+	i = 0;
+	while (*(s + i) != '\0')
+	{
+		j = 0;
+		while (j < 15)
+		{
+			if ((*(s + i) == *(arr + j)) || (*(s + i) == *(arr + j + 1)))
+			{
+				*(s + i) = *(arr + j + 2);
+				break;
+			}
+			else
+			{
+				j += 3;
+			}
+		}
+		i++;
+	}
+	return (s);
+}
