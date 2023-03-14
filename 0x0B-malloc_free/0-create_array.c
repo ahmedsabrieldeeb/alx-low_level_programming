b0cca29e9b8417e71e85f80b29dd5d0bc22625b0
ahmedisdeep <eldeeb757575@gmail.com>
2023-03-14 11:04:45 +0200

    fix a test case

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,7 +15,9 @@ char *create_array(unsigned int size, char c)
 {
 	char *ptr;
 	unsigned int i;
-
+	
+	if (size == 0)
+		return (NULL);
 	ptr = malloc(sizeof(char) * size);
 	if (ptr == NULL)
 		return (ptr);
