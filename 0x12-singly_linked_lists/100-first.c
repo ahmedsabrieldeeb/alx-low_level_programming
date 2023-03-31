49002416fc6ccc901a783ab5218cc62d368c74e1
ahmedisdeep <eldeeb757575@gmail.com>
2023-03-31 07:59:07 +0200

    edit foramtting

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
+void print_statement(void) __attribute__((constructor));
+
 /**
  * print_statement - a function to print before main()
  *
  * Return: void
  */
-
-void print_statement(void) __attribute__((constructor));
-
 void print_statement(void)
 {
 	printf("You're beat! and yet, you must \
