2b63cfd991ee44a4a80a7db1c3cf969e7eb905d8
ahmedisdeep <eldeeb757575@gmail.com>
2023-03-31 07:54:00 +0200

    print before  main

---
+++ 0x12-singly_linked_lists/test/100-first.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * print_statement - a function to print before main()
+ *
+ * Return: void
+ */
+
+void print_statement(void) __attribute__((constructor));
+
+void print_statement(void)
+{
+	printf("You're beat! and yet, you must \
+	allow,\nI bore my house upon my back!\n");
+}
