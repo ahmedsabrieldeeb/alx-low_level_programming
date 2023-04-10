8c6d70f70b989ecf7d926e66fd0fa0478efe13e3
ahmedisdeep <eldeeb757575@gmail.com>
2023-04-11 00:53:54 +0200

    find equivalent bit of specific index from an int

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * get_bit -  a function that returns the value of a bit at a given index
+ * @n: the integer to be traversed
+ * @index: the index of bit being searched for
+ *
+ * Return: the value of the bit, -1 if error happens
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int i = 0;
+	unsigned long int temp;
+
+	while (i <= index)
+	{
+		temp = n >> 1;
+		if (n - (temp * 2) == 0)
+		{
+			if (i == index)
+				return (0);
+		}
+		else
+		{
+			if (i == index)
+				return (1);
+		}
+		n = temp;
+		i++;
+	}
+	return (-1);
+}
