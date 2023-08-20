afb34f18cad24d1a79ea033c7840905a4bcbf912
Ahmed Eldeeb <eldeeb757575@gmail.com>
2023-08-21 00:03:32 +0300

    insert_dnodeint_at_index() func: second attempt

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -39,10 +39,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		if (order == idx)
 		{
-			temp->next->next->prev = new_node;
-			new_node->next = temp->next->next;
-			new_node->prev = temp;
-			temp->next = new_node;
+			temp->prev->next = new_node;
+			new_node->prev = temp->prev;
+			new_node->next = temp;
+			temp->prev = new_node;
 			return (new_node);
 		}
 		order++;
