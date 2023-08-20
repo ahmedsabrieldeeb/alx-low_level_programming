417a912bd4eef338386d18e13e844fa1b1b34793
Ahmed Eldeeb <eldeeb757575@gmail.com>
2023-08-21 00:58:15 +0300

    insert_dnodeint_at_index() func: consider idx is 0

--- 0x17-doubly_linked_lists/test/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/test/7-insert_dnodeint.c
@@ -37,6 +37,14 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	/* Traverse searching for index */
 	while (temp != NULL)
 	{
+		if (idx == 0)
+		{
+			temp->prev = new_node;
+			new_node->next = temp;
+			new_node->prev = NULL;
+			*h = new_node;
+			return (new_node);
+		}
 		if (order == idx)
 		{
 			temp->prev->next = new_node;
