7fc91c5f89f3b1343d755da4ec76112c5cd3aa42
Ahmed Eldeeb <eldeeb757575@gmail.com>
2023-08-21 01:08:14 +0300

    insert_dnodeint_at_index() func: the end issue

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -59,7 +59,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 
 	/* inserting at the end */
-	if (idx == order + 1)
+	if (idx == order)
 	{
 		new_node->next = NULL;
 
