bfdf730626b43aae2c28ff3c2430c00216ef528a
Ahmed Eldeeb <eldeeb757575@gmail.com>
2023-08-21 20:27:20 +0300

    again

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
-#!/usr/bash
+#!/bin/bash
 wget -P /temp https://github.com/ahmedsabrieldeeb/alx-low_level_programming/tree/master/0x18-dynamic_libraries/my_rand.so
 export LD_PRELOAD=/temp/my_rand.so
