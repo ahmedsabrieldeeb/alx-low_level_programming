79539f2e814d515ae9c7fcca851d9e1907c64065
Ahmed Eldeeb <eldeeb757575@gmail.com>
2023-08-21 03:14:36 +0300

    adjusting pointers

--- 0x17-doubly_linked_lists/test/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/test/8-delete_dnodeint.c
@@ -32,6 +32,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 			if (index == 0)
 			{
 				*head = temp->next;
+				temp->next->prev = NULL;
 				free(temp);
 				return (1);
 			}
