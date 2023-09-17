62646e6f0a899dd6923f61419ec45231d750dfe4
Ahmed Eldeeb <eldeeb757575@gmail.com>
2023-09-17 21:40:26 +0300

    delete

---
+++ 0x10-variadic_functions/1-main.c
@@ -0,0 +1,13 @@
+#include <stddef.h>
+#include "variadic_functions.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_numbers("; ", 5, 30, 938, -123024, 43402, 0);
+    return (0);
+}
